bff3ce9b00dd996b02d2aec4df1bc659a6d84e66
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-04-10 05:05:19 +0200

    add

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-process.on('unhandledRejection',(err)=>{
-    console.log('error',err);
-    
-})
+
 import  bootstrap  from "./Src/bootstrap.js";
 import express from 'express'
 export const app = express()
@@ -15,3 +12,4 @@ app.get('/', (req, res) => res.send('Hello World In Student System '))
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+export default app
