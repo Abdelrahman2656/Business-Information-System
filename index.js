c5966955f5647c89abbddb898730be062f058819
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-04-10 04:27:46 +0200

    add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT||3001
 
 
 bootstrap(app, express)
-
+export default app
 app.get('/', (req, res) => res.send('Hello World In Student System '))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
