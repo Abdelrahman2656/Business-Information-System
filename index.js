d369bd77efdf45433801fe46d4335778f7df2140
Abdelrahman2656 <a.elmonged870@gmail.com>
2025-04-10 04:14:12 +0200

    add

--- index.js
+++ index.js
@@ -12,8 +12,5 @@ const port = process.env.PORT||3001
 bootstrap(app, express)
 
 app.get('/', (req, res) => res.send('Hello World In Student System '))
-process.on('unhandledRejection',(err)=>{
-    console.log('error',err);
-    
-})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
