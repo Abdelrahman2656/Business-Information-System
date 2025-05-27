18c97c696fbbb5d14223f45d9eb7829acb258c93
Abdelrahaman Elmonged <142105484+Abdelrahman2656@users.noreply.github.com>
2025-05-27 16:38:26 +0300

    add

--- Src/Utils/Email/email.js
+++ Src/Utils/Email/email.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 })
 await transporter.sendMail({
     to,
-    from:"Business Information System<info@hti.edu.eg>",
+    from:"Business Information System 2025",
     subject,
     html,
     attachments
