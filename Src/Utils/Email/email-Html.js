87255e5e51d58bf46f27139de9fe97f03bb368d7
Abdelrahaman Elmonged <142105484+Abdelrahman2656@users.noreply.github.com>
2025-05-29 01:46:27 +0300

    add

--- Src/Utils/Email/email-Html.js
+++ Src/Utils/Email/email-Html.js
@@ -11,6 +11,7 @@ export const emailHtml = ( customId , password) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"/>
         <title>Student Credentials</title>
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
@@ -125,10 +126,8 @@ background-image: url('https://drive.google.com/uc?export=view&id=17DdMj8Mhrstg6
                   justify-content: center;
                   margin-right: 16px;
                 ">
-                  <svg xmlns="https://drive.google.com/file/d/16drch0AwjkCr-_hRP_FbGTDA0a44KWyl/view?usp=sharing" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#1a3a6c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                  </svg>
+              <i class="fa-solid fa-lock" style="font-size: 20px; color: #1a3a6c; width: 20px; height: 20px; display: inline-block;"></i>
+
                 </div>
                 <h2 style="
                   margin: 0;
