4e3e18afdc43aeeb32a852436e6a76628a01a41e
Abdelrahaman Elmonged <142105484+Abdelrahman2656@users.noreply.github.com>
2025-05-30 22:06:00 +0300

    add

--- Src/Utils/Email/email-Html.js
+++ Src/Utils/Email/email-Html.js
@@ -162,11 +162,7 @@ export const emailHtml = (customId, password) => {
               border-left: 3px solid #e9b949;
             ">
               <div style="display: flex; align-items: center;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#e9b949" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
-                  <line x1="12" y1="9" x2="12" y2="13"></line>
-                  <line x1="12" y1="17" x2="12.01" y2="17"></line>
-                </svg>
+              
                 <p style="
                   margin: 0 0 0 8px;
                   font-size: 13px;
